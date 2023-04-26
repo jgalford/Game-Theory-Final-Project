c4345c27826421c9f39786d6aacb29f41a88b35b
jgalford <jalford@zincpartners.us>
2023-04-26 08:15:42 -0400

    added mixed strats

--- game_sim.py
+++ game_sim.py
@@ -187,15 +187,84 @@ def find_top_10_pure_strats(runs):
 
     return top_10_strats
 
+def run_game_mixed(runs, percent, mixed1, mixed2, all_strats):
+    runs1 = int(runs*percent)
+    #print(f"Runs1: {runs1}")
+    runs2 = int(runs*(1-percent))
+    #print(f"Runs2: {runs2}")
+    total_win_rate = 0
+    total_score = 0
+    num_strats = len(all_strats)
+
+    for strat2 in all_strats:
+        if runs1 > 0:
+            win_rate1, score1, _, _, _, _ = run_game(runs1, mixed1, strat2, False)
+            total_win_rate += win_rate1
+            total_score += score1
+        if runs2 > 0:
+            win_rate2, score2, _, _, _, _ = run_game(runs2, mixed2, strat2, False)
+            total_win_rate += win_rate2
+            total_score += score2
+
+    avg_win_rate = (total_win_rate/2)/num_strats
+    avg_score = (total_score/2)/num_strats
+
+    return avg_win_rate, avg_score, percent, mixed1, mixed2
+
+def find_best_mixed(iterator, runs):
+    choices = [['low', 'middle', 'high'],
+               ['in|yes', 'out|yes'],
+               ['in|no', 'out|no'],
+               ['in|p1_in_&_prime', 'out|p1_in_&_prime'],
+               ['in|p1_out_&_prime', 'out|p1_out_&_prime'],
+               ['in|p1_in_&_composite', 'out|p1_in_&_composite'],
+               ['in|p1_out_&_composite', 'out|p1_out_&_composite']]
+    all_pure_strats = list(itertools.product(*choices))
+
+    top_10_strats = [('middle', 'out|yes', 'in|no', 'in|p1_in_&_prime', 'in|p1_out_&_prime', 'in|p1_in_&_composite', 'in|p1_out_&_composite'),
+                     ('middle', 'out|yes', 'in|no', 'in|p1_in_&_prime', 'in|p1_out_&_prime', 'in|p1_in_&_composite', 'out|p1_out_&_composite'),
+                     ('high', 'in|yes', 'out|no', 'in|p1_in_&_prime', 'in|p1_out_&_prime', 'in|p1_in_&_composite', 'in|p1_out_&_composite'),
+                     ('middle', 'out|yes', 'in|no', 'in|p1_in_&_prime', 'in|p1_out_&_prime', 'out|p1_in_&_composite', 'in|p1_out_&_composite'),
+                     ('high', 'in|yes', 'out|no', 'in|p1_in_&_prime', 'in|p1_out_&_prime', 'in|p1_in_&_composite', 'out|p1_out_&_composite'),
+                     ('middle', 'out|yes', 'in|no', 'in|p1_in_&_prime', 'in|p1_out_&_prime', 'out|p1_in_&_composite', 'out|p1_out_&_composite'),
+                     ('middle', 'out|yes', 'in|no', 'out|p1_in_&_prime', 'in|p1_out_&_prime', 'in|p1_in_&_composite', 'in|p1_out_&_composite'),
+                     ('high', 'in|yes', 'out|no', 'in|p1_in_&_prime', 'in|p1_out_&_prime', 'out|p1_in_&_composite', 'in|p1_out_&_composite'),
+                     ('low', 'in|yes', 'in|no', 'in|p1_in_&_prime', 'in|p1_out_&_prime', 'in|p1_in_&_composite', 'in|p1_out_&_composite'),
+                     ('middle', 'in|yes', 'in|no', 'in|p1_in_&_prime', 'in|p1_out_&_prime', 'in|p1_in_&_composite', 'in|p1_out_&_composite')]
+    
+    mixed_strat_set = list(itertools.combinations(top_10_strats, 2))
+    #print(mixed_strat_set[0][0])
+    #print(mixed_strat_set[0][1])
+
+    top_10_mixed_strats = [([None], [None], 0, 0, 0)] * 10
+    curr_progress = 1
+
+    def update_best_mixed_strats(strat1, strat2, percent1, win_rate, score):
+        for idx, (s1, s2, p, wr, sc) in enumerate(top_10_mixed_strats):
+            if score > sc:
+                    top_10_mixed_strats.insert(idx, (strat1, strat2, percent1, win_rate, score))
+                    top_10_mixed_strats.pop(-1)
+                    break
+            
+    for strat1, strat2 in mixed_strat_set:
+        for i in range(iterator):
+            percent = (i/iterator)
+            result = run_game_mixed(runs, percent, strat1, strat2, all_pure_strats)
+            update_best_mixed_strats(strat1, strat2, percent, result[0], result[1])
+        print('Processing: ' + str(curr_progress) + ' of ' + str(len(mixed_strat_set)))
+        curr_progress += 1
+
+    return top_10_mixed_strats
+
 if __name__ == "__main__":
     #EX: p1_strat = ['low', 'in|yes', 'in|no', 'in|p1_in_&_prime', 'in|p1_in_&_composite', 'in|p1_out_&_prime', 'in|p1_out_&_composite']
     #EX: p2_strat = ['low', 'in|yes', 'in|no', 'in|p1_in_&_prime', 'in|p1_in_&_composite', 'in|p1_out_&_prime', 'in|p1_out_&_composite']
     #p1_strat = random_strat()
     #p2_strat = random_strat()
-    p1_strat = ['middle', 'out|yes', 'in|no', 'in|p1_in_&_prime', 'in|p1_out_&_prime', 'in|p1_in_&_composite', 'in|p1_out_&_composite']
-    p2_strat = ['high', 'in|yes', 'out|no', 'in|p1_in_&_prime', 'in|p1_out_&_prime', 'in|p1_in_&_composite', 'in|p1_out_&_composite']
+    #p1_strat = ['middle', 'out|yes', 'in|no', 'in|p1_in_&_prime', 'in|p1_out_&_prime', 'in|p1_in_&_composite', 'in|p1_out_&_composite']
+    #p2_strat = ['high', 'in|yes', 'out|no', 'in|p1_in_&_prime', 'in|p1_out_&_prime', 'in|p1_in_&_composite', 'in|p1_out_&_composite']
 
-    run_game(1000000, p1_strat, p2_strat, True)
+    #run_game(1000000, p1_strat, p2_strat, True)
 
     # top_10_pure_strats = find_top_10_pure_strats(10000)
     # with open('best_pure_strats.txt', 'w') as f:
@@ -203,13 +272,8 @@ if __name__ == "__main__":
     #         print(f"{idx + 1}. Strategy: {strat}, Win Rate: {win_rate}, Average Score: {score}")
     #         f.write(f"{idx + 1}. Strategy: {strat}, Win Rate: {win_rate}, Average Score: {score}" + '\n')
 
-
-
-    # pure_strat = find_best_pure_strat()
-    # best_pure_strat = pure_strat[0]
-    # best_pure_strat_win_rate = pure_strat[1]
-    # best_pure_strat_score = pure_strat[2]
-
-    # print(f"Best pure strategy: {best_pure_strat}")
-    # print(f"Best pure strategy win rate: {best_pure_strat_win_rate}")
-    # print(f"Best pure strategy score: {best_pure_strat_score}")
+    top_10_mixed_strats = find_best_mixed(10, 10000)
+    with open('best_mixed_strats.txt', 'w') as f:
+        for idx, (strat1, strat2, percent1, win_rate, score) in enumerate(top_10_mixed_strats):
+            print(f"{idx + 1}. Strategy 1: {strat1}, Strategy 2: {strat2}, Percent Strategy 1: {percent1}, Win Rate: {win_rate}, Average Score: {score}")
+            f.write(f"{idx + 1}. Strategy 1: {strat1}, Strategy 2: {strat2}, Percent Strategy 1: {percent1}, Win Rate: {win_rate}, Average Score: {score}" + '\n')
